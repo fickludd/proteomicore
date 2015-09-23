b1e0308dc210d31cd2861f1bfcd6514aee1cb634
Johan Teleman <johan.teleman@immun.lth.se>
2015-09-23 14:05:32 +0200

    Implemented a,c,x and z peptide fragments

--- Proteins/src/main/java/se/lth/immun/chem/EPeptideFragment.java
+++ Proteins/src/main/java/se/lth/immun/chem/EPeptideFragment.java
@@ -4,9 +4,16 @@ public enum EPeptideFragment {
 	a('a'), b('b'), c('c'), x('x'), y('y'), z('z');
 	
 	public final char type;
+	public static final double A_MASS_DIFF = Element.O.monoisotopicWeight + Element.C.monoisotopicWeight;
+	public static final double X_MASS_DIFF = Element.O.monoisotopicWeight + Element.C.monoisotopicWeight - 2*Element.H.monoisotopicWeight;
+	public static final double CZ_MASS_DIFF = Element.N.monoisotopicWeight + 3*Element.H.monoisotopicWeight;
 	
 	private EPeptideFragment(char type) {
 		this.type = type;
+	}
+	
+	public boolean isNTerm() {
+		return type == 'a' || type == 'b' || type == 'c';
 	}
 	
 	public static EPeptideFragment fromChar(char ch) {
--- Proteins/src/main/java/se/lth/immun/chem/PeptideFragment.java
+++ Proteins/src/main/java/se/lth/immun/chem/PeptideFragment.java
@@ -2,12 +2,12 @@ package se.lth.immun.chem;
 
 public class PeptideFragment implements IMolecule {
 
-	public char	fragmentType;
-	public int		ordinal;
-	public double	mass;
-	public Peptide peptide;
+	public EPeptideFragment		fragmentType;
+	public int					ordinal;
+	public double				mass;
+	public Peptide 				peptide;
 	
-	public PeptideFragment(char fragmentType, int ordinal, double mass, Peptide peptide) {
+	public PeptideFragment(EPeptideFragment fragmentType, int ordinal, double mass, Peptide peptide) {
 		this.fragmentType = fragmentType;
 		this.ordinal = ordinal;
 		this.mass = mass;
@@ -22,15 +22,19 @@ public class PeptideFragment implements IMolecule {
 	public boolean same(PeptideFragment pf) {
 		return ordinal == pf.ordinal && fragmentType == pf.fragmentType;
 	}
+	
+	public String toString() {
+		return "PF("+fragmentType.type + ordinal+", m="+mass+")";
+	}
 
 	private IsotopeDistribution _dist = null;
 	public IsotopeDistribution getIsotopeDistribution() {
 		if (_dist == null) {
 			_dist = new IsotopeDistribution();
-			if (fragmentType == 'b') {
+			if (fragmentType == EPeptideFragment.b) {
 				for (int i = 0; i < ordinal; i++)
 					_dist.add(peptide.aminoAcids[i].getIsotopeDistribution());
-			} else if (fragmentType == 'y') {
+			} else if (fragmentType == EPeptideFragment.y) {
 				for (int i = 0; i < ordinal; i++)
 					_dist.add(peptide.aminoAcids[peptide.aminoAcids.length - i - 1].getIsotopeDistribution());
 			}
